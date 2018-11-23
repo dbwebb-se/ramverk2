2683751c45338ebc39e97dec7331ff66d7afdc34
Emil Folino <emilfolino@gmail.com>
2018-11-23 09:53:51 +0100

    Minified version of vanilla quote

--- example/quote/vanilla/main.js
+++ example/quote/vanilla/main.js
@@ -1,28 +1,30 @@
 (function () {
-    let root = document.getElementById('root');
+    "use strict";
 
-    let messageBoard = document.createElement("div");
+    var root = document.getElementById('root');
+
+    var messageBoard = document.createElement("div");
 
     messageBoard.className = "message-board";
     messageBoard.textContent = "No message yet!";
 
     root.appendChild(messageBoard);
 
-    let fetchMessageButton = document.createElement("button");
+    var fetchMessageButton = document.createElement("button");
 
     fetchMessageButton.className = "fetch-message-button";
     fetchMessageButton.textContent = "Fetch quote of the day";
 
-    fetchMessageButton.addEventListener("click", () => {
+    fetchMessageButton.addEventListener("click", function() {
         fetch('http://quotes.rest/qod.json')
             .then(function(response) {
                 return response.json();
             })
             .then(function(result) {
-                let quote = result.contents.quotes[0].quote;
-                let author = result.contents.quotes[0].author;
+                var quote = result.contents.quotes[0].quote;
+                var author = result.contents.quotes[0].author;
 
-                messageBoard.innerHTML = `<h1>"${quote}"</h1><p>- ${author}</p>`;
+                messageBoard.innerHTML = "<h1>\"" + quote + "\"</h1><p>- " + author + "</p>";
 
                 root.removeChild(fetchMessageButton);
             });
